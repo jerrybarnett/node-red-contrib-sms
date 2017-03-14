d1b2e8c3b5e633d20214f889d6af615c3211001a
Jerry Barnett <Jerry Barnett>
2017-03-14 13:42:51 -0400

    Final first run,  Working as expected

--- sms/sms.js
+++ sms/sms.js
@@ -1,24 +1,25 @@
-module.exports = function(RED) {
-    function sms(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        this.on('input', function(msg) {
+module.exports = function (RED) {
+  function SMSNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
 
-            msg.method ="GET";
-            msg.payload="";
-            //use http or https depends how server is configured
-            var protocol = "http";
-            var username = this.credentials.username;
-            var password = this.credentials.password;
-            var ip = this.ip
-            var port = this.port;
-            var recipient = this.recipient;
-            var message = this.message;
-            var myurl =  protocol+"://"+username+":"+password+"@"+ip+":"+port+"/send.html?smsto="+recipient+"&smsbody="+message;
-            msg.url=myurl;
-            node.send(msg);
+    this.on('input', function (msg) {
 
-        });
-    }
-    RED.nodes.registerType("sms",SMSNode);
+      msg.method = "GET";
+      msg.payload = "";
+      var protocol = "http";
+      this.username = config.username;
+      this.password = config.password;
+      this.ip = config.ip;
+      this.port = config.port;
+      this.recipient = config.recipient;
+      this.message = config.message;
+
+      var myurl = protocol + "://" + this.username + ":" + this.password + "@" + this.ip + ":" + this.port + "/send.html?smsto=" + this.recipient + "&smsbody=" + this.message;
+      msg.url = myurl;
+      node.send(msg);
+
+    });
+  }
+  RED.nodes.registerType("sms", SMSNode);
 }
